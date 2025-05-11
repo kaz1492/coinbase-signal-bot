5c14e779640e7be329b8f83e0feaadc1fd9b6409
kaz1492 <91811068+kaz1492@users.noreply.github.com>
2025-05-11 05:04:27 -0500

    Delete websocket_listener.py

--- websocket_listener.py
+++
@@ -1,3 +0,0 @@
-# Optional: This module can be filled if real-time WebSocket tracking is needed.
-# For Coinbase Pro WebSocket listener setup:
-# https://docs.cloud.coinbase.com/exchange/docs/websocket-overview
